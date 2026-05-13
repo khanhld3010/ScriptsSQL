f6f995c09835b39d817dcd042e45bef870df67b1
khanhld3010 <luuduckhanh3010@gmail.com>
2026-05-13 14:49:54 +0700

    update

--- Script-2.sql
+++ Script-2.sql
@@ -10,4 +10,7 @@ SELECT * FROM class c WHERE MONTH(c.StartDate) = 12;
 SELECT * FROM subject s WHERE s.Credit BETWEEN 3 AND 5;
 
 -- Thay đổi mã lớp(ClassID) của sinh viên có tên ‘Hung’ là 2
-ALTER TABLE student 
+UPDATE student s SET ClassID = 2 WHERE s.StudentName = "hung";
+
+-- Hiển thị các thông tin: StudentName, SubName, Mark. Dữ liệu sắp xếp theo điểm thi (mark) giảm dần. nếu trùng sắp theo tên tăng dần.
+SELECT s.StudentName , sbj.SubName , m.Mark  FROM student s JOIN mark m ON s.StudentID = m.StudentID JOIN subject sbj ON m.SubID = sbj.SubID ORDER BY m.Mark DESC , s.StudentName ASC ;
